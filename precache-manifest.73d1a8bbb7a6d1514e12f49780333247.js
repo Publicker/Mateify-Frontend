33f1456ee3ce4808a97c75a57481c64854c638a4
Publicker <public.maldonado@gmail.com>
2020-10-04 22:26:46 -0300

    Updates

---
+++ precache-manifest.73d1a8bbb7a6d1514e12f49780333247.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "464dc6095e546c809f1665727cd2f9ec",
+    "url": "/Mateify-Frontend/index.html"
+  },
+  {
+    "revision": "dd7697f5f871c7d3184c",
+    "url": "/Mateify-Frontend/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "f2b4c0685f2b49839045",
+    "url": "/Mateify-Frontend/static/js/2.3ad52937.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Mateify-Frontend/static/js/2.3ad52937.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd7697f5f871c7d3184c",
+    "url": "/Mateify-Frontend/static/js/main.93e22301.chunk.js"
+  },
+  {
+    "revision": "2e4b198697578b2a5733",
+    "url": "/Mateify-Frontend/static/js/runtime-main.366cdd32.js"
+  }
+]);
